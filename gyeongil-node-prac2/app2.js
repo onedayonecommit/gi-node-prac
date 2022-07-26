ba573c4e1d671349a50850364e45a087d2bb9e2d
onedayonecommit <rudghks0981@gmail.com>
2022-07-26 15:14:52 +0900

    generate success chatroom

--- gyeongil-node-prac2/app2.js
+++ gyeongil-node-prac2/app2.js
@@ -9,5 +9,24 @@ const PORT = 8080;
 const server = app.listen(PORT, () => {
     console.log(8080,"server start!")
 })
-
 const io = socketio(server)
+
+app.get("/", (req, res) => {
+    fs.readFile("page2.html","utf-8",(err,data) => {
+        res.send(data);
+    })
+})
+
+
+// io.sockets.on("connection") 클라이언트가 접속했을 때
+// io.sockets.on("disconnect") 클라이언트가 종료했을 때
+
+io.sockets.on("connection", (socket) => {
+    // 클라이언트에서 socket.emit("message",data);
+    // 웹소켓에 연결되어있는 message 이벤트를 실행시켜준다.
+    // 밑에 코드
+    socket.on("message", (data) => {
+        //요기
+        io.sockets.emit("message", data);
+    });
+});
