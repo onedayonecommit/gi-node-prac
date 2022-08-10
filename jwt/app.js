43ff080aa7f9c54ae5c7f812c258ed107ca31d24
onedayonecommit <rudghks0981@gmail.com>
2022-08-10 10:23:53 +0900

    session study

--- jwt/app.js
+++ jwt/app.js
@@ -15,11 +15,19 @@ const fs = require("fs")
 const mysql = require("mysql2");
 const session = require("express-session");
 const { reverse } = require("dns");
-const filestore = require("session-file-store")(session); // 세션 파일 스토어를 부르고 바로 함수 실행
+const { create } = require("domain");
+// const filestore = require("session-file-store")(session); // 세션 파일 스토어를 부르고 바로 함수 실행
 
-app.use(bodyparser())
+let temp = mysql.createConnection({
+    host: "localhost",
+    password: "rudghks110",
+    database: "jwt",
+    user: "root"
+})
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(
     session({
+        key: "rudghks09",
         // 세션을 발급할 때 사용되는 키 소스코드 노출 안되게 env에 담아서 사용
         secret: process.env.SESSION_KEY,
         // 세션을 저장하고 불러올 때 다시 저장할지 여부
@@ -27,26 +35,60 @@ app.use(
         // 세션에 저장할 때 초기화 여부
         saveUninitialized: true,
         // 저장소를 만들지 여부
-        store: new filestore()
+        // store: new filestore(),
     })
 );
+app.set('view engine', 'ejs');
+app.set('views',"./views")
 
 app.listen(process.env.PORT, () => {
     console.log(process.env.PORT + "server start")
 })
 
 app.get("/", (req, res) => {
-    // fs.readFile("index.html", "utf-8", (err, result) => {
-    //     res.send(result);
-    // })
-    if (!req.session.key) {
-        req.session.key = "rudhaskfj1k23"
+    temp.query("select * from members where id = '"+req.body.id+"';", (err, result) => {
+        if (err) console.log(err)
+    })
+    console.log(req.session.user_id)
+    if (req.session.num === undefined) {
+        req.session.num = 1
     }
-    res.send(`key:${req.session.key}`);
+    else req.session.num++
+    if (req.session.user_id === undefined) {
+        res.render("index")
+    }
+    else {
+        res.render("logout", {
+            id:req.session.user_id
+        })
+    }
 })
 
 app.post("/login2", (req, res) => {
-    res.send(req.body.id + req.body.pw)
+    const signquery = "insert into members (id, pw) values (?,?) "
+    const checkquery = "select id from members where id = '"
+        temp.query(signquery, [req.body.id, req.body.pw], (err, result) => {
+            if (err) {
+                console.log("쿼리문 에러", err)
+            }
+            else {
+                temp.query(checkquery + req.body.id + "';", (err, result2) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    else {
+                        console.log(result2)
+                        req.session.user_id = result2[0].id
+                        console.log(req.session.user_id)
+                            res.redirect("/")
+                    }
+                })
+            }
+        })
+    })
+
+app.get("/already", (req, res) => {
+    res.render("already")
 })
 
 app.get("/shop", (req, res) => {
@@ -54,7 +96,25 @@ app.get("/shop", (req, res) => {
 })
 
 app.get("/login", (req, res) => {
-    fs.readFile("index2.html", "utf-8", (err, result) => {
-        res.send(result)
-    })
+    res.render("index2")
+})
+
+app.get("/success", (req, res) => {
+        res.cookie("rudghks's cookie", session, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 20, // 쿠키 발급 후 만료시간 밀리초 단위;
+        })
+    res.render("success")
+})
+
+app.post("/logout", (req, res) => {
+    req.session.destroy(res.redirect("/"));
+})
+
+app.get("/session", (req,res) => {
+    if (req.session.count) {
+        req.session.count++;
+        res.send("hi")
+    }
+    else res.redirect("/")
 })
